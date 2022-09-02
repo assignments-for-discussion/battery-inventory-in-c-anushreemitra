58694af0a668756535514ac0fb266c21ee76ed08
Anushree Mitra <71174019+anushreemitra@users.noreply.github.com>
2022-09-02 19:01:50 +0530

    refactor: countBatteriesByUsage()
    Added code and comments to count the low/medium/high number of batteries

--- bucketer.c
+++ bucketer.c
@@ -9,6 +9,16 @@ struct CountsByUsage {
 
 struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
   struct CountsByUsage counts = {0, 0, 0};
+  
+  //iterating over the chargeCycleCounts array for the number of batteries present in it
+  for(int i=0; i<nBatteries; i++){
+    if(cycles[i]<400) // increments the number of low counts only if it is less than 400
+        counts.lowCount+=1; // accesses and increments the struct variables
+    else if(cycles[i]>400 && cycles[i]<=919) //increments the number of medium counts if it is in given range
+        counts.mediumCount+=1;
+    else
+        counts.highCount+=1; // increments the number of high counts counts only if it is more than or equal to 920
+  }
   return counts;
 }
 
